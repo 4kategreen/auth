6d24693b17603da703e6c3feb071782d8a82ce6a
Kate Green <4kategreen@gmail.com>
2019-02-02 15:55:57 -0500

    slightly better handlers and errors.

--- index.js
+++ index.js
@@ -20,33 +20,52 @@ http
           console.error(err);
         });
 
-        // better error handling needed
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        let result = handleRequest(req);
 
-        res.write(handleRequest(req));
+        res.writeHead(result.status, {'Content-Type': 'text/html'});
+
+        // probably some better granularity here, but works for now
+        if (result.status < 300) {
+          res.write(result.body);
+        }
         res.end();
       });
     })
-    .listen(3000);
+    .listen(3000, () => {
+      console.log(`Server running`)
+    });
 
+// hot mess
 let handleRequest = (req) => {
-  let body = `<h2>${req.method} ${req.url}</h2>`;
+  let body = `<h2>${req.method} ${req.url}</h2>`,
+      status = 404;
 
   switch (req.url) {
+    // surely there's a better way to deal with this. 
+    case '/favicon.ico':
+      status = 200;
+      break;
     case '/auth':
       switch (req.method) {
         case 'GET':
           body+= '<p>Get existing token</p>';
+          status = 200;
           break;
         case 'POST':
           body+= '<p>Submit new creds</p>';
+          status = 200;
           break;
       }
       break;
-    default:
-      body+= '<p>Unhandled request</p>';
+    case '/status':
+      switch (req.method) {
+        case 'GET':
+          body+= '<p>Status result</p>';
+          status = 200;
+          break;
+      }
       break;
   }
 
-  return body;
+  return { status: status, body: body };
 }
