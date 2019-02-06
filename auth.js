c5e6caa379eb5d7a72253d5005eefc13b6c77092
Kate Green <4kategreen@gmail.com>
2019-02-06 12:21:05 -0500

    hook up `signature` module to the handlers

--- auth.js
+++ auth.js
@@ -1,45 +1,48 @@
-let handler = (req, res) => {
-  return {
-    body: '<p>Make new token</p>',
-    status: 200
+const signature = require('./signature');
+
+let handler = (req) => {
+  if (verifyUser) {
+    let token = signature.generate(req);
+    returnSuccess(token);
+  } else {
+    returnFail();
   }
 };
 
 let status = (params) => {
-  let token = params.getAll('token');
+  let token = params.token,
+      verified = false,
+      status = 401;
+
   if (token.length === 1) {
-    // query the database
+    verified = signature.verify(token);
+  }
+
+  if (verified) {
+    returnSuccess(token);
   } else {
-    // error ffs
+    returnFail();
   }
-  return {
-    body: `<p>Verify existing token: ${params}</p>`,
-    status: 200
-  }
+};
+
+let returnSuccess = (token) => {
+  res.writeHead(200, {
+    'content-type': 'text/html',
+    'authorization': token
+  });
+};
+
+let returnFail = () => {
+  res.writeHead(401, {
+    'content-type': 'text/html'
+  });
+};
+
+let verifyUser = () => {
+
 };
 
 module.exports = {
   handler: handler,
   status: status
-}
-
-/***
-POST /auth
-purpose: given valid login credentials, create token
-  compare against database
-  return true
-    create token
-    send token
-    200
-  return false
-    401
-GET /auth
-purpose: compare token to db so app can verify login
-  assess token
-  return true
-    continue on your way
-    200
-  return false
-    redirect
-    401
-***/
+};
