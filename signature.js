725d3307939aadcde13c0d2fa5246df6b787f7af
Casey <casey@rampantmonkey.com>
2019-02-25 15:44:32 -0500

    use asymmetric keypair for signing tokens

--- signature.js
+++ signature.js
@@ -1,4 +1,4 @@
-const CryptoJS = require('crypto-js')
+const crypto = require('crypto')
 
 const base64url = (src) => {
   var srcBuffer = Buffer.from(JSON.stringify(src), 'utf8')
@@ -6,30 +6,49 @@ const base64url = (src) => {
   return encodedSrc
 }
 
-const sign = (msg) => {
-  const hmac = CryptoJS.HmacSHA256(msg, 'secret-goes-here')
-  return hmac.toString(CryptoJS.enc.Base64).replace(/=+$/, '')
+const tokenHeader = { "alg": "RS256", "typ": "JWT" }
+
+const buildSign = (privateKey) => {
+  return (msg) => {
+    const s = crypto.createSign('SHA256')
+    s.write(msg)
+    s.end()
+    return s.sign(privateKey, 'base64')
+  }
 }
 
-const tokenHeader = { "alg": "HS256", "typ": "JWT" }
-
-const generate = (payload) => {
-  let msg = base64url(tokenHeader) + '.' + base64url(payload)
-  let signature = sign(msg)
-  let token = msg + '.' + signature
-  return token
+const buildGenerate = (sign) => {
+  return (payload) => {
+    let msg = base64url(tokenHeader) + '.' + base64url(payload)
+    let signature = sign(msg)
+    let token = msg + '.' + signature
+    return token
+  }
 }
 
-const verify = (token) => {
-  let components = token.split('.', 3)
-  if(components.length !== 3) { return false }
-  let msg = components[0] + '.' + components[1]
-  let providedSignature = components[2]
-  let computedSignature = sign(msg)
-  return providedSignature === computedSignature
+const buildVerify = (publicKey) => {
+  return (token) => {
+    let components = token.split('.', 3)
+    if(components.length !== 3) { return false }
+    let msg = components[0] + '.' + components[1]
+    let providedSignature = components[2]
+    let v = crypto.createVerify('SHA256')
+    v.write(msg)
+    v.end()
+    return v.verify(publicKey, providedSignature, 'base64')
+  }
 }
 
-module.exports = {
-  generate: generate,
-  verify: verify
+const build = (privateKey, publicKey) => {
+  return {
+    generate: buildGenerate(buildSign(crypto.createPrivateKey(privateKey))),
+    verify: buildVerify(crypto.createPublicKey(publicKey)),
+    publicKey: publicKey
+  }
+}
+
+module.exports = (options = {}) => {
+  if(options.privateKey && options.publicKey) {
+    return build(options.privateKey, options.publicKey)
+  }
 }
